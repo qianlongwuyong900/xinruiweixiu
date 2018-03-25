1b37f04f24e1f40c1a27e22c1b30db8b414e71c1
qianlongwuyong900 <2246149620@qq.com>
2018-03-25 21:00:03 +0800

    初始化项目

---
+++ src/main/java/com/xinrui/xinruiweixiu/XinruiweixiuApplication.java
@@ -0,0 +1,12 @@
+package com.xinrui.xinruiweixiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XinruiweixiuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XinruiweixiuApplication.class, args);
+	}
+}
---
+++ src/test/java/com/xinrui/xinruiweixiu/XinruiweixiuApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xinrui.xinruiweixiu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XinruiweixiuApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
